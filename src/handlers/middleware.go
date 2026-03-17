fc2fbb6e09f9461ce6ae5e7cf8359cf5da284a13
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-17 17:16:31 +0530

    feat: integrate src/handlers/middleware.go

---
+++ src/handlers/middleware.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
+}
